d40beebbab48c99775f4dabe082dbc2b2a8eb573
itishreya <shreyaiyer1998@gmail.com>
2020-10-01 21:49:23 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$('#one').hover(function() {
+     $('#bgeffect').addClass('active1');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active1');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#two').hover(function() {
+     $('#bgeffect').addClass('active2');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active2');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#three').hover(function() {
+     $('#bgeffect').addClass('active3');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active3');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#four').hover(function() {
+     $('#bgeffect').addClass('active4');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active4');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#five').hover(function() {
+     $('#bgeffect').addClass('active5');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active5');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#six').hover(function() {
+     $('#bgeffect').addClass('active6');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active6');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#seven').hover(function() {
+     $('#bgeffect').addClass('active7');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active7');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#eight').hover(function() {
+     $('#bgeffect').addClass('active8');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active8');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#nine').hover(function() {
+     $('#bgeffect').addClass('active9');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active9');
+     $('#bgeffect').addClass('always');
+});
+
+
+$('#ten').hover(function() {
+     $('#bgeffect').addClass('active10');
+     $('#bgeffect').removeClass('always');
+}, function() {
+     $('#bgeffect').removeClass('active10');
+     $('#bgeffect').addClass('always');
+});
